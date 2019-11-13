29c4a44b944277d4ec601c74867ad1741985a1f9
Nikku Shankar <Nikku.Shankar@outlook.com>
2019-11-12 23:28:57 -0800

    Add Mock Service for testing

---
+++ src/test/java/MockService.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+
+import fi.iki.elonen.NanoHTTPD;
+
+public class MockService extends NanoHTTPD  {
+
+	public MockService(int port) throws IOException {
+		super(port);		
+		start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+	}
+
+	public static void main(String[] args) throws IOException {
+		MockService service = new MockService(7070);
+	}
+
+	@Override
+	public Response serve(IHTTPSession arg0) {
+		JsonObjectBuilder attrBuilder = Json.createObjectBuilder();
+		attrBuilder.add("price", "100.00").add("volume", "50000");
+		
+		JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
+		jsonBuilder.add("type", "QUOTE")
+		.add("identifier", "AAPL")
+		.add("attributes", attrBuilder);
+		
+		Response response = newFixedLengthResponse(jsonBuilder.build().toString()); 
+	    response.addHeader("Content-Type","application/json");
+	    return response;
+	}
+}
